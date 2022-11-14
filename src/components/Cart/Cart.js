3fdc0edbeebabeaa14ea5875c806aa38f96093c8
gazisharinmostafiz <sharin.mostafiz@gmail.com>
2022-11-14 16:01:31 +0600

    update 1.3

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './Cart.css'
-const Cart = ({cart}) => {
+const Cart = (props) => {
+    const {cart} = props;
+    // console.log(cart);
+    let total = 0;
+    for(const product of cart){
+        total = total + product.price;
+    }
+
     return (
         <div className='cart'>
              <h3>Order summary</h3>
             <p>Selected Items: {cart.length}</p>
-            <p>Total Price : </p>
+            <p>Total Price : ${total} </p>
             <p>Total Shipping:</p>
             <p>Tax:</p>
             <h5>Grand Total:</h5>
