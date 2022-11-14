7ff78ea8f2eb6c788504397935b4771b363f26f3
gazisharinmostafiz <sharin.mostafiz@gmail.com>
2022-11-14 16:07:35 +0600

    update 1.4

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,17 +4,20 @@ const Cart = (props) => {
     const {cart} = props;
     // console.log(cart);
     let total = 0;
+    let shipping = 0;
     for(const product of cart){
         total = total + product.price;
+        shipping = shipping + product.shipping;
     }
+    const tax = (total * (10 /100)).toFixed(2); // decimal fixed
 
     return (
         <div className='cart'>
              <h3>Order summary</h3>
             <p>Selected Items: {cart.length}</p>
             <p>Total Price : ${total} </p>
-            <p>Total Shipping:</p>
-            <p>Tax:</p>
+            <p>Total Shipping: ${shipping}</p>
+            <p>Tax: {tax}</p>
             <h5>Grand Total:</h5>
         </div>
     );
