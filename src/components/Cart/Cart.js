27ae33c51bc0c70874582574f93e38a7871d1646
gazisharinmostafiz <sharin.mostafiz@gmail.com>
2022-11-14 15:55:54 +0600

    update 1.2

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,14 @@
 import React from 'react';
-
+import './Cart.css'
 const Cart = ({cart}) => {
     return (
-        <div>
+        <div className='cart'>
              <h3>Order summary</h3>
             <p>Selected Items: {cart.length}</p>
+            <p>Total Price : </p>
+            <p>Total Shipping:</p>
+            <p>Tax:</p>
+            <h5>Grand Total:</h5>
         </div>
     );
 };
